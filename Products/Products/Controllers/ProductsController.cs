04a2fd2bd74fd41dfdca596957e282fef1098e46
nurai <coder.nurai@gmail.com>
2023-05-05 16:34:44 +0530

    Products File

---
+++ Products/Products/Controllers/ProductsController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Products.Repositories;
+
+namespace Products.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly IProduct _product;
+
+        public ProductsController(IProduct product)
+        {
+            _product = product;
+        }
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var products = _product.GetProducts();
+            return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Products/Products/DbContext/DapperContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Products.DbContext
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        public IDbConnection CreateConnection()
+        {
+            var connectionString = _configuration.GetConnectionString("ProductsDb");
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
