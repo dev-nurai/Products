cc638cd52fa2af02b20112e3fbb40b31b0476346
Nurai <118501054+dev-nurai@users.noreply.github.com>
2023-05-05 18:37:39 +0530

    Delete Products directory

--- Products/Products/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Products.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
